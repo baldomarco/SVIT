0b9ef6b9adb43ac85d5fc08c276fe30a73eaf169
baldomarco <92460568+baldomarco@users.noreply.github.com>
2022-05-09 20:58:32 +0200

    Update SVIT.r

--- SVIT.r
+++ SVIT.r
@@ -32,13 +32,13 @@ names(dmp.wg) <- c("Jan 99"," Jan 00"," Jan 01"," Jan 02"," Jan 03","Jan 04","Ja
 # Used library
 library(rasterVis)
 
-# DMP images multiple time serie
+# DMP images multiple time serie Fig 3a
 levelplot(dmp.wg, 
           names=c("Jan 99"," Jan 00"," Jan 01"," Jan 02"," Jan 03","Jan 04","Jan 05","Jan 06","Jan 07","Jan 08","Jan 09","Jan 10","Jan 11",
                   "Jan 12","Jan 13","Jan 14","Jan 15","Jan 16","Jan 17","Jan 18","Jan 19","Jan 20")
          )
 
-# DMP multiple time series boxplot visualization
+# DMP multiple time series boxplot visualization Fig 3b
 boxplot(dmp.wg, 
         outline=F, 
         horizontal=F, 
@@ -66,13 +66,13 @@ fapar.wg <- crop(fapar.multi,ext)
 names(fapar.wg) <- c("Jan 99"," Jan 00"," Jan 01"," Jan 02"," Jan 03","Jan 04","Jan 05","Jan 06","Jan 07","Jan 08","Jan 09","Jan 10","Jan 11",
                      "Jan 12","Jan 13","Jan 14","Jan 15","Jan 16","Jan 17","Jan 18","Jan 19","Jan 20")
 
-# FAPAR images multiple time serie
+# FAPAR images multiple time serie Fig 3a
 levelplot(fapar.wg, 
           names=c("1999","2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012","2013","2014","2015","2016",
                   "2017","2018","2019","2020")
          )
 
-# FAPAR multiple time series boxplot visualization
+# FAPAR multiple time series boxplot visualization Fig 3b
 boxplot(fapar.wg, 
         outline=F, 
         horizontal=F, 
@@ -100,11 +100,11 @@ ndvi.wg <- crop(ndvi.multi,ext)
 names(ndvi.wg) <- c("Jan 99"," Jan 00"," Jan 01"," Jan 02"," Jan 03","Jan 04","Jan 05","Jan 06","Jan 07","Jan 08","Jan 09","Jan 10","Jan 11",
                     "Jan 12","Jan 13","Jan 14","Jan 15","Jan 16","Jan 17","Jan 18","Jan 19","Jan 20")
 
-# NDVI images multiple time serie
+# NDVI images multiple time serie Fig 3a
 levelplot(ndvi.wg, names=c("1999","2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012","2013","2014","2015",
                            "2016","2017","2018","2019","2020"))
 
-# NDVI multiple time series boxplot visualization
+# NDVI multiple time series boxplot visualization Fig 3b
 boxplot(ndvi.wg, 
         outline=F, 
         horizontal=F, 
@@ -114,7 +114,7 @@ boxplot(ndvi.wg,
         col="gold"
        )
 
-#______________________________________________________________________________________________
+#______________________________________________________________________________________________ (Tab. S1)
 # Computing the medians of each products of the datasets
 # DMP
 summary(dmp.wg)
@@ -126,23 +126,7 @@ summary(fapar.wg)
 summary(ndvi.wg)
 
 
-#________________________________________________________________________________________________
-# Pearson's Correlation Analysis amoung our satellite cropped for the Central Western Ghat (CWG) products
-
-# Used R required packages
-library(GGally)
-
-# Pearson's Correlation Analyses amoung DMP dataset CWG products
-ggpairs(dmp.wg)
-
-# Pearson's Correlation Analyses amoung FAPAR dataset CWG products
-ggpairs(fapar.wg)
-
-# Pearson's Correlation Analyses amoung NDVI dataset CWG products
-ggpairs(ndvi.wg)
-
-#_____________________________________________________     ###### PCA #######     ___________________________________________________________________
-
+#_____________________________________________________     ###### PCA #######     ___________________________________________________________________ (Tab. S2)
 
 
 # raster PCA analyzes all the DMP images products to modelling raster objects in a single one evaluating a single component (DMP index in this case)
@@ -160,8 +144,23 @@ ndviPCA <- rasterPCA(ndvi.wg)
 summary(ndviPCA$model)                                                          
 plot(ndviPCA$map)
 
+#_____________________________________________________________________________________________________________________________________(Fig. S1-2-3)
+# Pearson's Correlation Analysis amoung our satellite cropped for the Central Western Ghat (CWG) products
 
-## Builds the three data frame used for the statistical validation (https://github.com/baldomarco/SVIT/blob/main/statistics_validation.r)
+# Used R required packages
+library(GGally)
+
+# Pearson's Correlation Analyses amoung DMP dataset CWG products
+ggpairs(dmp.wg)
+
+# Pearson's Correlation Analyses amoung FAPAR dataset CWG products
+ggpairs(fapar.wg)
+
+# Pearson's Correlation Analyses amoung NDVI dataset CWG products
+ggpairs(ndvi.wg)
+
+#________________________________________________________________________________________________________________________________________
+## Builds the three data frame used for the statistical validation
 # Used required R packages
 library(tidyverse)
 
@@ -174,7 +173,7 @@ stc <- stack(
   fn
 )
 
-stc_df <- fortify(dmp.wg, maxpixels = 5689958400) %>% 
+stc_df_dmp <- fortify(dmp.wg, maxpixels = 5689958400) %>% 
   pivot_longer(
     .,
     cols = -(1:2),
@@ -183,8 +182,11 @@ stc_df <- fortify(dmp.wg, maxpixels = 5689958400) %>%
 
 
 #___________________Data frame structure_________________
-stc_df$value
-str(stc_df)
+stc_df_dmp
+
+stc_df_dmp$value
+
+str(stc_df_dmp)
 
 
 #___________________FAPAR data frame_____________________
@@ -196,7 +198,7 @@ stc <- stack(
   fn
 )
 
-stc_df2 <- fortify(fapar.wg, maxpixels = 5689958400) %>% 
+stc_df_fapar <- fortify(fapar.wg, maxpixels = 5689958400) %>% 
   pivot_longer(
     .,
     cols = -(1:2),
@@ -204,11 +206,11 @@ stc_df2 <- fortify(fapar.wg, maxpixels = 5689958400) %>%
   )
 
 #___________________Data frame structure_________________
-stc_df2
+stc_df_fapar
 
-str(stc_df2)
+stc_df_fapar$value
 
-stc_df$value
+str(stc_df_fapar)
 
 
 #___________________NDVI data frame_____________________
@@ -220,7 +222,7 @@ stc <- stack(
   fn
 )
 
-stc_df3 <- fortify(ndvi.wg, maxpixels = 5689958400) %>% 
+stc_df_ndvi <- fortify(ndvi.wg, maxpixels = 5689958400) %>% 
   pivot_longer(
     .,
     cols = -(1:2),
@@ -228,18 +230,20 @@ stc_df3 <- fortify(ndvi.wg, maxpixels = 5689958400) %>%
   )
 
 #___________________Data frame structure_________________
-stc_df3$value
+stc_df_ndvi
 
-str(stc_df3)
+stc_df_ndvi$value
 
+str(stc_df_ndvi)
 
+#______________________Linear regression models and additional statistics computation___________________
 
-#_____________Linear regression models___________________
+#  ------- Residuals vs Fitted ----- Normal Q-Q ----- Scale Location ----- Residuals vs Levarege ----- Cook's Distance
 
-#_______________________DMP______________________________
+#_______________________________________DMP______________________________
 
-plot(stc_df)
-mod1 <- lm(stc_df)
+plot(stc_df_dmp)
+mod1 <- lm(stc_df_dmp)
 mod1
 summary(mod1)
 plot(mod1)
@@ -247,8 +251,8 @@ plot(mod1)
 
 #_______________________FAPAR___________________________
 
-plot(stc_df2)
-mod2 <- lm(stc_df2)
+plot(stc_df_fapar)
+mod2 <- lm(stc_df_fapar)
 mod2
 summary(mod2)
 plot(mod2)
@@ -256,14 +260,14 @@ plot(mod2)
 
 #________________________NDVI___________________________
 
-plot(stc_df3)
-mod3 <- lm(stc_df3)
+plot(stc_df_ndvi)
+mod3 <- lm(stc_df_ndvi)
 mod3
 summary(mod3)
 plot(mod3)
 #abline(a = mod1$coefficients[1], b = mod1$coefficients[2], col = "red", lwd = 3)
 
-#____________________________________________________________________________________________________________
+#______________________________________________________________________________________________
 # Computing the medians of each products of the datasets
 # DMP
 summary(dmp.wg)
@@ -299,7 +303,7 @@ Precipitation <- c(5987, 4675, 5492, 5522, 5473, 5371, 5043, 5415, 3907, 4271, 4
 time<- c(1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 
          2014, 2015, 2016, 2017, 2018, 2019)
 
-#____________________________________________________________________________________________________________________________
+#___________________________________________________________________________________________________________________________________________ Fig. 3c
 # Create the Time ~ Medians data frame to use ggplot visualization for the 3 indices
 # Used required R packges 
 library(ggplot2)
@@ -472,7 +476,7 @@ boxplot(NDVI10,
 
 
 #____________________________________Difference and NDVI trends (Uppangala Area)__________________________________________
-#_____________________________________6 years Reserve NDVI differential Analyses__________________________________________
+#_____________________________________6 years Reserve NDVI differential Analyses__________________________________________  Fig. 4
 
 dif <- q2021- q2016
 
@@ -530,8 +534,7 @@ boxplot(NDVI10,
        )      
 
 
-
-# Cropping the NDVI S2-L2A differences with Kadamakal Reserve Forest and Pushpagiri Wildlife Sanctuary Shape File (trakking borders)
+# Cropping the NDVI S2-L2A differences with Kadamakal Reserve Forest and Pushpagiri Wildlife Sanctuary Shape File (trakking borders) Fig. 5
 # NDVI Total difference 2021-2016
 dup <- spTransform(up.shp, proj4string(dif))
 duptot <- mask(crop(dif, extent(dup)), dup)
