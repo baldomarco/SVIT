d4ca643ae7934e0122056df48db9bc6f27a790d1
baldomarco <92460568+baldomarco@users.noreply.github.com>
2022-07-31 11:43:39 +0200

    Update SVIT.r

--- SVIT.r
+++ SVIT.r
@@ -34,7 +34,7 @@ names(dmp.wg) <- c("Jan 99"," Jan 00"," Jan 01"," Jan 02"," Jan 03","Jan 04","Ja
 # Used library
 library(rasterVis)
 
-# DMP images multiple time serie Fig 3a
+# DMP images multiple time serie Fig 3a    gift -> https://oscarperpinan.github.io/rastervis/
 levelplot(dmp.wg, 
           names=c("1999","2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012","2013","2014","2015","2016",
                   "2017","2018","2019","2020")
