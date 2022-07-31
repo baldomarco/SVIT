f8a0a2a3a4d84505e5d2bb21b45955d4138a3112
baldomarco <92460568+baldomarco@users.noreply.github.com>
2022-07-31 02:12:22 +0200

    Update SVIT.r

--- SVIT.r
+++ SVIT.r
@@ -1,7 +1,9 @@
-# Satellite Vegetation Indices Trend (SVIT) is an Algorithm created by the Ph.D. student Baldo Marco to analyze the forest changes in terms of cover and productivity. 
-# Together with PhD Piero Zannini, PhD student Kurian Ayushi and the Full Professor of the University of Bologna Duccio Rocchini, 
-# We create an R script to analize the Dry Matter Productivity, Fraction of Absorbed Photosintetically Active Radiation and Normalized Different Vegetation Index trends of a selected area, in this case over the Western Ghats world Biodiversity Hotspot.
+# Satellite Vegetation Indices Trend (SVIT) is an algorithm structure created by the Ph.D. candidate Baldo Marco to analyze the forest changes in terms of vegetation cover, photosinthesis and primary productivity. 
+# Together with Piero Zannini, PhD, Kurian Ayushi, PhD and Full Professor Duccio Rocchini, PhD, together we elaborated an R script to analize the Dry Matter Productivity (DMP), 
+# Fraction of Absorbed Photosintetically Active Radiation (FAPAR) and Normalized Different Vegetation Index (NDVI) trends of a selected area. 
+# In our case over the central portion of the Western Ghats global biodiversity hotspot and within the Kadamakal Reserve Forest and Pushpagiri Wildlife Sanctuary borders.
 # We realized this analysis under the theory of the multispectral bands analysis and multiple time series analysis.
+
 # Multiple Time Series Analysis on DMP, FAPAR, NDVI V2 1Km spatial scale from VITO catalogues 1999-2020 of the WG region
 
 library(ncdf4)
